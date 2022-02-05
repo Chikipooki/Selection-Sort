380d39eb7aacd21e36639c4d360ba499766be76c
Chikipooki <homesotelittle@gmail.com>
2022-02-05 21:46:35 +0700

    Added full project SelectSort

---
+++ Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TSVPS_SelectSort
+{
+    class Program
+    {
+        private static List<int> elementsArray = new List<int>(); 
+
+        private static void Print()
+        {
+            for (int i = 0; i < elementsArray.Count; i++)
+            {
+                Console.Write($"{elementsArray[i]} ");
+            }
+        }
+
+        private static void GetRandomElements(int amountOfElements)
+        {
+            Random randomElements = new Random();
+            for (int i = 0; i < amountOfElements; i++)
+            {
+                elementsArray.Add(randomElements.Next(0, 100));
+            }
+        }
+
+        private static void SelectSort()
+        {
+            int minIdx = 0; int minItemFound = 0;
+            for (int correntElementIdx = 0; correntElementIdx < elementsArray.Count; correntElementIdx++)
+            {
+                minIdx = correntElementIdx;
+                for (int remainingElementIdx = correntElementIdx + 1;  remainingElementIdx < elementsArray.Count;  remainingElementIdx++)
+                {
+                    if (elementsArray[remainingElementIdx] < elementsArray[minIdx])
+                    {
+                        minIdx = remainingElementIdx;
+                    }
+                }
+                // To view indexes of changed elements (Для просмотра индексов меняемых элементов)
+                //Console.WriteLine("Меняем элемент " + correntElementIdx + " на " + minIdx);
+                minItemFound = elementsArray[minIdx];
+                elementsArray[minIdx] = elementsArray[correntElementIdx];
+                elementsArray[correntElementIdx] = minItemFound;
+            }                         
+        } 
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Сортировка массива методом SelectSort");
+            Console.Write("Введите кол-во элементов массива: ");
+            int amountOfElements = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"\nНе отсортироваванный массив на {amountOfElements} элементов");
+            GetRandomElements(amountOfElements);
+            Print();
+            Console.WriteLine();
+            
+            SelectSort();
+
+            Console.WriteLine($"Отсортироваванный массив на {amountOfElements} элементов");
+            Print();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("TSVPS_SelectSort")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TSVPS_SelectSort")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("a9ad88ae-8008-47f1-90d6-b7bb259c4ad7")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
